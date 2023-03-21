af6fc53ada2d016c6b6c4c2ad51d85f55c7f9a02
Dmytro <dm.kalitventsev@gmail.com>
2023-03-21 17:47:44 +0200

    implemented a restart of the game

--- js/main.js
+++ js/main.js
@@ -1,66 +1,90 @@
 let doc = document;
 
 let elementField = doc.querySelectorAll('.element-field');
+let playerWalks = doc.querySelector('.player-walk');
 let resultGame = doc.querySelector('.result-game');
+let btnResetGame = doc.querySelector('.reset-game');
 
-let playerTurn = 'o';
-let gX = `<div class="game-element_x"></div>`;
-let playerO = `<div class="game-element_o"></div>`;
+let playerTurn = true;
+let playerX = 'Player X';
+let playerO = 'Player O';
+let gameElementX = `<div class="game-element_x"></div>`;
+let gameElementO = `<div class="game-element_o"></div>`;
 let counterElement = 0;
 
-for (let i = 0; i < elementField.length; i++) {
-	elementField[i].onclick = function () {
-		if (this.innerHTML) {
-			return;
+startGame();
+resetGame();
+
+function startGame() {
+	for (let i = 0; i < elementField.length; i++) {
+		elementField[i].innerHTML = '';
+
+		elementField[i].onclick = function () {
+			if (this.innerHTML) {
+				return;
+			}
+
+			playerWalks.innerText = playerX;
+
+			if (playerTurn) {
+				this.innerHTML = gameElementX;
+				playerWalks.innerText = playerO;
+			} else {
+				this.innerHTML = gameElementO;
+				playerWalks.innerText = playerX;
+			}
+
+			playerTurn = !playerTurn;
+
+			counterElement++;
+
+			showResultGame();
 		}
-
-		if (playerTurn == 'o') {
-			playerTurn = 'x';
-			this.innerHTML = playerX;
-		} else {
-			playerTurn = 'o';
-			this.innerHTML = playerO;
-		}
-
-		counterElement++;
-
-		getResultGame();
 	}
+
+	playerTurn = true;
+	counterElement = 0;
+	resultGame.innerText = '';
+	playerWalks.innerText = '';
+}
+
+function resetGame() {
+	btnResetGame.onclick = startGame;
 }
 
 function checkWinner() {
-	if (elementField[0].innerHTML == playerX && elementField[1].innerHTML == playerX && elementField[2].innerHTML == playerX) {
-		return 'player X';
-	} else if (elementField[3].innerHTML == playerX && elementField[4].innerHTML == playerX && elementField[5].innerHTML == playerX) {
-		return 'player X';
-	} else if (elementField[6].innerHTML == playerX && elementField[7].innerHTML == playerX && elementField[8].innerHTML == playerX) {
-		return 'player X';
-	} else if (elementField[0].innerHTML == playerX && elementField[3].innerHTML == playerX && elementField[6].innerHTML == playerX) {
-		return 'player X';
-	} else if (elementField[1].innerHTML == playerX && elementField[4].innerHTML == playerX && elementField[7].innerHTML == playerX) {
-		return 'player X';
-	} else if (elementField[2].innerHTML == playerX && elementField[5].innerHTML == playerX && elementField[8].innerHTML == playerX) {
-		return 'player X';
-	} else if (elementField[0].innerHTML == playerX && elementField[4].innerHTML == playerX && elementField[8].innerHTML == playerX) {
-		return 'player X';
-	} else if (elementField[2].innerHTML == playerX && elementField[4].innerHTML == playerX && elementField[6].innerHTML == playerX) {
-		return 'player X';
-	} else if (elementField[0].innerHTML == playerO && elementField[1].innerHTML == playerO && elementField[2].innerHTML == playerO) {
-		return 'player O';
-	} else if (elementField[3].innerHTML == playerO && elementField[4].innerHTML == playerO && elementField[5].innerHTML == playerO) {
-		return 'player O';
-	} else if (elementField[6].innerHTML == playerO && elementField[7].innerHTML == playerO && elementField[8].innerHTML == playerO) {
-		return 'player O';
-	} else if (elementField[0].innerHTML == playerO && elementField[3].innerHTML == playerO && elementField[6].innerHTML == playerO) {
-		return 'player O';
-	} else if (elementField[1].innerHTML == playerO && elementField[4].innerHTML == playerO && elementField[7].innerHTML == playerO) {
-		return 'player O';
-	} else if (elementField[2].innerHTML == playerO && elementField[5].innerHTML == playerO && elementField[8].innerHTML == playerO) {
-		return 'player O';
-	} else if (elementField[0].innerHTML == playerO && elementField[4].innerHTML == playerO && elementField[8].innerHTML == playerO) {
-		return 'player O';
-	} else if (elementField[2].innerHTML == playerO && elementField[4].innerHTML == playerO && elementField[6].innerHTML == playerO) {
-		return 'player O';
+	if (elementField[0].innerHTML == gameElementX && elementField[1].innerHTML == gameElementX && elementField[2].innerHTML == gameElementX) {
+		return playerX;
+	} else if (elementField[3].innerHTML == gameElementX && elementField[4].innerHTML == gameElementX && elementField[5].innerHTML == gameElementX) {
+		return playerX;
+	} else if (elementField[6].innerHTML == gameElementX && elementField[7].innerHTML == gameElementX && elementField[8].innerHTML == gameElementX) {
+		return playerX;
+	} else if (elementField[0].innerHTML == gameElementX && elementField[3].innerHTML == gameElementX && elementField[6].innerHTML == gameElementX) {
+		return playerX;
+	} else if (elementField[1].innerHTML == gameElementX && elementField[4].innerHTML == gameElementX && elementField[7].innerHTML == gameElementX) {
+		return playerX;
+	} else if (elementField[2].innerHTML == gameElementX && elementField[5].innerHTML == gameElementX && elementField[8].innerHTML == gameElementX) {
+		return playerX;
+	} else if (elementField[0].innerHTML == gameElementX && elementField[4].innerHTML == gameElementX && elementField[8].innerHTML == gameElementX) {
+		return playerX;
+	} else if (elementField[2].innerHTML == gameElementX && elementField[4].innerHTML == gameElementX && elementField[6].innerHTML == gameElementX) {
+		return playerX;
+	} else if (elementField[0].innerHTML == gameElementO && elementField[1].innerHTML == gameElementO && elementField[2].innerHTML == gameElementO) {
+		return playerO;
+	} else if (elementField[3].innerHTML == gameElementO && elementField[4].innerHTML == gameElementO && elementField[5].innerHTML == gameElementO) {
+		return playerO;
+	} else if (elementField[6].innerHTML == gameElementO && elementField[7].innerHTML == gameElementO && elementField[8].innerHTML == gameElementO) {
+		return playerO;
+	} else if (elementField[0].innerHTML == gameElementO && elementField[3].innerHTML == gameElementO && elementField[6].innerHTML == gameElementO) {
+		return playerO;
+	} else if (elementField[1].innerHTML == gameElementO && elementField[4].innerHTML == gameElementO && elementField[7].innerHTML == gameElementO) {
+		return playerO;
+	} else if (elementField[2].innerHTML == gameElementO && elementField[5].innerHTML == gameElementO && elementField[8].innerHTML == gameElementO) {
+		return playerO;
+	} else if (elementField[0].innerHTML == gameElementO && elementField[4].innerHTML == gameElementO && elementField[8].innerHTML == gameElementO) {
+		return playerO;
+	} else if (elementField[2].innerHTML == gameElementO && elementField[4].innerHTML == gameElementO && elementField[6].innerHTML == gameElementO) {
+		return playerO;
 	}
 }
 
@@ -70,18 +94,18 @@ function finishGame() {
 	}
 }
 
-function getResultGame() {
+function showResultGame() {
 	let winner = checkWinner();
 
 	if (winner) {
 		finishGame();
 	}
 
-	if (winner == 'player X') {
-		resultGame.innerHTML = 'Player X wins!';
-	} else if (winner == 'player O') {
-		resultGame.innerHTML = 'Player O wins!';
+	if (winner == playerX) {
+		resultGame.innerText = `${playerX} wins!`;
+	} else if (winner == playerO) {
+		resultGame.innerText = `${playerO} wins!`;
 	} else if (counterElement == elementField.length) {
-		resultGame.innerHTML = 'Draw!';
+		resultGame.innerText = 'Draw!';
 	}
 }
